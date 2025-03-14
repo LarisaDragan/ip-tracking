3963cf1a70e0a13969d78aa997bf32b8afa30d76
LarisaDragan <dlarisa.maria@yahoo.com>
2025-03-14 12:14:24 +0200

    update config for github pipelines

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/ip-tracking",
   plugins: [react()],
+  define: {
+    "process.env": {},
+  },
+  resolve: {
+    alias: {
+      crypto: path.resolve(__dirname, "node_modules/crypto-browserify"),
+    },
+  },
 });
